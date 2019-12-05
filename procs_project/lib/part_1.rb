89be848882d7402ba4a423f88d5e2f6dfb9a1446
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-04 22:38:20 -0500

    another solution to procs project part 1

--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -10,18 +10,49 @@ def my_select(arr, &prc)
     new_arr
 end
 
+# def my_count(arr, &prc)
+#     my_select(arr, &prc).count
+# end
+
 def my_count(arr, &prc)
-    my_select(arr, &prc).count
+    count = 0 
+    arr.each do |ele|
+        if prc.call(ele)
+            count += 1 
+        end 
+    end 
+    count
 end
+
+# def my_any?(arr, &prc)
+#     my_count(arr, &prc) > 0 
+# end
 
 def my_any?(arr, &prc)
-    my_count(arr, &prc) > 0 
-end
+    arr.each do |ele|
+        return true if prc.call(ele)
+    end
+    false
+end 
+
+# def my_all?(arr, &prc)
+#     my_count(arr, &prc) == arr.length
+# end
 
 def my_all?(arr, &prc)
-    my_count(arr, &prc) == arr.length
-end
+    arr.each do |ele|
+        return false if !prc.call(ele)
+    end
+    true
+end 
+
+# def my_none?(arr, &prc)
+#     !my_any?(arr, &prc)
+# end 
 
 def my_none?(arr, &prc)
-    !my_any?(arr, &prc)
+    arr.each do |ele|
+        return false if prc.call(ele)
+    end
+    true
 end 
