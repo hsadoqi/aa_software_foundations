5c0beaa0be8c0b0b03cb11d49a32e28727acffbc
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-08 22:56:41 -0500

    finished startup project part 1

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -2,4 +2,32 @@ require "employee"
 
 class Startup
 
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end 
+
+    def name 
+        @name
+    end 
+
+    def funding 
+        @funding
+    end
+
+    def salaries 
+        @salaries 
+    end
+
+    def employees 
+        @employees 
+    end 
+
+    def valid_title?(title)
+        salaries[title]
+    end
+
+
 end
