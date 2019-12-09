5be7e44d00560356cb5ad29b5e9b53f64b2a1199
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-08 23:07:49 -0500

    finished part 2 and started part 3

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -26,8 +26,41 @@ class Startup
     end 
 
     def valid_title?(title)
-        salaries[title]
+        salaries[title] ? true : false
     end
+
+    def >(startup)
+        funding > startup.funding
+    end
+
+    def hire(employee_name, title)
+        if !salaries[title]
+            raise "Title is not valid"
+        else 
+            employees << Employee.new(employee_name, title)
+        end
+    end
+
+    def size 
+        employees.length
+    end
+
+    def pay_employee(employee)
+        employee_pay = salaries[employee.title]
+
+        if funding > employee_pay
+            employee.pay(employee_pay)
+            @funding -= employee_pay
+        else 
+            raise "No more money"
+        end
+    end
+
+    def payday
+        @employees.map do |employee|
+            pay_employee(employee)
+        end
+    end 
 
 
 end
