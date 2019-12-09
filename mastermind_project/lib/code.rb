1d183b3eced7902f74979d6c637757dad9beaf8e
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-09 11:53:43 -0500

    finished part 1 and part 2 of mastermind project

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,4 +6,49 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(chars)
+    chars.all? {|char| Code::POSSIBLE_PEGS[char.upcase]}
+  end
+
+  attr_reader :pegs
+
+  def initialize(chars)
+    if !Code.valid_pegs?(chars)
+      raise "those chars are not valid"
+    else 
+      @pegs = chars.map(&:upcase)
+    end
+  end
+
+  def self.random(length)
+    pegs = Array.new(length) {Code::POSSIBLE_PEGS.keys.sample}
+    Code.new(pegs)
+  end
+
+  def self.from_string(str)
+    Code.new(str.split(""))
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length 
+    @pegs.length
+  end
+
+  def num_exact_matches(other_code)
+    (0...other_code.length).count { |i| other_code[i] == @pegs[i] }
+  end
+
+  def num_near_matches(other_code)
+    matches = (0...other_code.length).count { |i| @pegs.include?(other_code[i]) }
+
+    matches - num_exact_matches(other_code)
+  end
+
+  def ==(other_code)
+    self.pegs == other_code.pegs
+  end
+
 end
