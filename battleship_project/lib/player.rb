493fae14b9327b2cb531c74d7a3ec3782c0a0129
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-09 14:39:53 -0500

    finished battleship project

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
 
+    def get_move 
+        puts 'enter a position with coordinates separated with a space like `4 7`'
+        user_response = gets.chomp
+        user_response.split(" ").map(&:to_i)
+    end 
+
 end
